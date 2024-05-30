956f2e2f96ee47be3ed5670d12446018382bf7b3
manith-hetti <manithhetti@gmail.com>
2024-05-30 09:59:06 +0100

    comment functionality now works

--- src/main/java/com/makersacademy/acebook/controller/PostsController.java
+++ src/main/java/com/makersacademy/acebook/controller/PostsController.java
@@ -2,51 +2,73 @@ package com.makersacademy.acebook.controller;
 
 import com.makersacademy.acebook.model.Comment;
 import com.makersacademy.acebook.model.Post;
+import com.makersacademy.acebook.model.User;
 import com.makersacademy.acebook.repository.ICommentRepository;
 import com.makersacademy.acebook.repository.PostRepository;
+import com.makersacademy.acebook.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
-import java.util.List;
 import java.util.Optional;
 
 @Controller
 public class PostsController {
 
     @Autowired
-    PostRepository repository;
+    PostRepository postRepository;
 
     @Autowired
     ICommentRepository commentRepository;
 
+
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping("/posts")
     public String index(Model model) {
-        Iterable<Post> posts = repository.findAll();
+        Iterable<Post> posts = postRepository.findAll();
         model.addAttribute("posts", posts);
         model.addAttribute("post", new Post());
-
-        Iterable<Comment> comments = commentRepository.findAll();
-        model.addAttribute("comments", comments);
         model.addAttribute("comment", new Comment());
+
         return "posts/index";
     }
 
     @PostMapping("/posts")
     public RedirectView create(@ModelAttribute Post post) {
-        repository.save(post);
+        postRepository.save(post);
         return new RedirectView("/posts");
     }
 
     @PostMapping("/posts-like")
     public RedirectView like(@RequestParam("postId") Long postId) {
-        Optional<Post> postOptional = repository.findById(postId);
+        Optional<Post> postOptional = postRepository.findById(postId);
         Post post = postOptional.get();
         System.out.println(post);
         post.incrementLikeCount();
-        repository.save(post);
+        postRepository.save(post);
         return new RedirectView("/posts");
     }
+
+    @PostMapping("/posts-comments")
+    public RedirectView comment(@RequestParam("postId") Long postId, @RequestParam("content") String content) {
+        Optional<Post> postOptional = postRepository.findById(postId);
+        Post post = postOptional.get();
+        Comment comment = new Comment();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        String currentPrincipleName = authentication.getName();
+
+        comment.setContent(content);
+        comment.setPost(post);
+        comment.setUser_id(userRepository.findIdByUsername(currentPrincipleName));
+        commentRepository.save(comment);
+        return new RedirectView("/posts");
+    }
+
 }
--- src/main/java/com/makersacademy/acebook/repository/ICommentRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/ICommentRepository.java
@@ -1,9 +1,16 @@
 package com.makersacademy.acebook.repository;
 
 import com.makersacademy.acebook.model.Comment;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ICommentRepository extends CrudRepository<Comment, Long> {
+
+    @Query(value = "SELECT c FROM Comment c JOIN c.post p WHERE p.id = :postId")
+    Iterable<Comment> findCommentByPostId(@Param("postId") Long postId);
 }
+
+
